20b6d844b7e536410fb59bfa7bf7cdedfd0e21fb
CatchertheRye <125881435@qq.com>
2025-03-20 17:03:12 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,11 +98,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk1.cn"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 61116;
-pub const RELAY_PORT: i32 = 61117;
+pub const RELAY_PORT: i32 = 81117;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
